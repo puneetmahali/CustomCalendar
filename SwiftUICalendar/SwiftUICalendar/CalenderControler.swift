e2597c32466526936c85c557e9cc6bb27c8a13e2
Puneet Mahali <iospuneet@gmail.com>
2020-08-10 21:05:15 +0200

    Display Calendar Views.

---
+++ SwiftUICalendar/SwiftUICalendar/CalenderControler.swift
@@ -0,0 +1,61 @@
+//
+//  CalenderControler.swift
+//  SwiftUICalendar
+//
+//  Created by Puneet Mahali on 10.08.20.
+//  Copyright © 2020 Puneet Mahali. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct dayItem:Identifiable {
+    var id:Int
+    var dayNumber:Int
+    var dayName:String
+    var month:String
+    var year:Int
+}
+
+class CalenderController:UIResponder{
+   
+    
+       func generateDatesArrayBetweenTwoDates(startDate: Date, endDate: Date) -> [Date] {
+           var datesArray: [Date] = [Date]()
+           var startDate = startDate
+           let calendar = Calendar.current
+           while startDate <= endDate {
+               let tempDate = dateFormatter().string(from: startDate)
+               datesArray.append(dateFormatter().date(from: tempDate) ?? Date())
+               startDate = calendar.date(byAdding: .day, value: 1, to: startDate)!
+           }
+           return datesArray
+       }
+       func dateFormatter() -> DateFormatter {
+           let formatter = DateFormatter()
+           formatter.dateFormat = "MM/dd/yyyy"
+           return formatter
+       }
+       func getDates(startDate: Date, monthsAdded:Int)-> [dayItem]{
+      var listOfDays = [dayItem]()
+           let currentDate = startDate
+           var dateComponent = DateComponents()
+           dateComponent.month = monthsAdded
+        let futureDate = Calendar.current.date(byAdding: dateComponent, to: currentDate)!
+
+           let datesArray = generateDatesArrayBetweenTwoDates(startDate: currentDate, endDate: futureDate)
+        for (index,date) in datesArray.enumerated() {
+               let calendar = Calendar(identifier: .gregorian)
+            let components = calendar.dateComponents([.day,.weekday,.month,.year], from: date)
+            let dayname = dateFormatter().standaloneWeekdaySymbols[components.weekday! - 1]
+                  // .standaloneWeekdaySymbols[components.day as! Int % 7]
+            let day =  components.day!
+            let month = dateFormatter().standaloneMonthSymbols[components.month! - 1]
+            let year = components.year!
+               print("day \(dayname),\(day)")
+            
+            listOfDays.append(dayItem(id:index, dayNumber: day, dayName: dayname,month: month,year: year))
+           }
+        return listOfDays
+       }
+}
